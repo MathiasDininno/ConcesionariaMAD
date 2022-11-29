7d24b5915be3621f9f26d8d32403c0678ed865ad
Mathias <dininnom1@gmail.com>
2022-11-29 20:02:27 -0300

    Entrega cuatro

--- Entrega/estructuraHome.js
+++ Entrega/estructuraHome.js
@@ -1,212 +1,263 @@
-const autos = [
-    { id: 11, marca: "Audi", modelo: "A7", anio: 2022, kilometros: 0, estado: "Nuevo", valor: 15000000, img: "./img/Auto-Audi.png" },
-    { id: 12, marca: "Chevrolet", modelo: "Cruze", anio: 2022, kilometros: 0, estado: "Nuevo", valor: 6000000, img: "./img/Auto-Chevrolet.png" },
-    { id: 13, marca: "Ford", modelo: "Focus-SE", anio: 2022, kilometros: 0, estado: "Nuevo", valor: 6500000, img: "./img/Auto-Ford.png" },
-    { id: 14, marca: "Volkswagen", modelo: "Polo", anio: 2022, kilometros: 0, estado: "Nuevo", valor: 5500000, img: "./img/Auto-Volkswagen.png" },
-    { id: 15, marca: "Audi", modelo: "TT", anio: 2010, kilometros: 126000, estado: "Usado", valor: 8500000, img: "./img/Auto-Audi-TT.png" },
-    { id: 16, marca: "Chevrolet", modelo: "Corsa", anio: 2005, kilometros: 235000, estado: "Usado", valor: 860000, img: "./img/Auto-Chevrolet-Corsa.png" },
-    { id: 17, marca: "Ford", modelo: "EcoSport", anio: 2014, kilometros: 75000, estado: "Usado", valor: 1820000, img: "./img/Auto-Ford-EcoSport.png" },
-    { id: 18, marca: "Volkswagen", modelo: "Amarok", anio: 2015, kilometros: 123000, estado: "Usado", valor: 8820000, img: "./img/Auto-Volkswagen-Amarok.png" },
-]
+const url = "./autos.json"
 
+fetch(url)
+    .then(res => res.json())
+    .then(function autosJson(autos) {
 
-//---- FILTRAR POR MENOR VALOR----
+        // Politica y privacidad(Boton acordeon)
+        let coll = document.getElementsByClassName("collapsible");
+        let i;
 
-let menorValor = document.getElementById("menorValor")
- 
-menorValor.addEventListener("click", () => {
-    autos.sort((a, b) => {
-        if (a.valor < b.valor) {
-            return -1;
-        } else if (a.valor > b.valor) {
-            return 1;
-        } else {
-            return 0;
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                let content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
         }
-    })
-    renderizarAutos(autos)
-})
 
-// ----FILTRAR POR MAYOR VALOR ----
-let mayorValor = document.getElementById("mayorValor")
+        // Formulario de compra
+        const idFormulario = document.getElementById('formulario')
 
-mayorValor.addEventListener("click", () => {
-    autos.sort((a, b) => {
-        if (a.valor > b.valor) {
-            return -1;
-        } else if (a.valor < b.valor) {
-            return 1;
-        } else {
-            return 0;
+        idFormulario.addEventListener('submit', (e) => {
+            e.preventDefault()
+            Swal.fire('¡El pago se realizo con exito!')
+            eliminarProducto()
+        })
+
+        // Eliminar producto del carrito
+        const eliminarProducto = (id) => {
+            const producto = carritoCompra.find((producto) => producto.id === id)
+            carritoCompra.splice(carritoCompra.indexOf(producto), 1)
+            localStorage.setItem("carritoContenedor", JSON.stringify(carritoCompra))
+            retornarCarrito()
         }
-    })
-    renderizarAutos(autos)
-})
+
+        // Boton para vaciar carrito
+        const vaciarCarrito = document.getElementById("vaciarCarrito")
+
+        vaciarCarrito.addEventListener('click', () => {
+            carritoCompra.splice(0, carritoCompra.length);
+            localStorage.setItem("carritoContenedor", JSON.stringify(carritoCompra))
+            retornarCarrito()
+        })
+
+        //---- FILTRAR POR MENOR VALOR----
+        let menorValor = document.getElementById("menorValor")
+
+        menorValor.addEventListener("click", () => {
+            autos.sort((a, b) => {
+                if (a.valor < b.valor) {
+                    return -1;
+                } else if (a.valor > b.valor) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            renderizarAutos(autos)
+            agregarAlCarrito(autos)
+        })
+
+        // ----FILTRAR POR MAYOR VALOR ----
+        let mayorValor = document.getElementById("mayorValor")
+
+        mayorValor.addEventListener("click", () => {
+            autos.sort((a, b) => {
+                if (a.valor > b.valor) {
+                    return -1;
+                } else if (a.valor < b.valor) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            renderizarAutos(autos)
+            agregarAlCarrito(autos)
+        })
 
 
-// Vehiculo mayor kilometros
+        // Vehiculo mayor kilometros
 
-let mayorKilometros = document.getElementById("mayorKilometros")
+        let mayorKilometros = document.getElementById("mayorKilometros")
 
-mayorKilometros.addEventListener("click", () => {
-    autos.sort((a, b) => {
-        if (a.kilometros > b.kilometros) {
-            return -1;
-        } else if (a.kilometros < b.kilometros) {
-            return 1;
-        } else {
-            return 0;
+        mayorKilometros.addEventListener("click", () => {
+            autos.sort((a, b) => {
+                if (a.kilometros > b.kilometros) {
+                    return -1;
+                } else if (a.kilometros < b.kilometros) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            renderizarAutos(autos)
+            agregarAlCarrito(autos)
+        })
+
+
+        // Vehiculo menor kilometros
+
+        let menorKilometros = document.getElementById("menorKilometros")
+
+        menorKilometros.addEventListener("click", () => {
+            autos.sort((a, b) => {
+                if (a.kilometros < b.kilometros) {
+                    return -1;
+                } else if (a.kilometros > b.kilometros) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            renderizarAutos(autos)
+            agregarAlCarrito(autos)
+        })
+
+
+        // Vehiculo mayor kilometros
+
+        let menorAnio = document.getElementById("menorAnio")
+
+        menorAnio.addEventListener("click", () => {
+            autos.sort((a, b) => {
+                if (a.anio < b.anio) {
+                    return -1;
+                } else if (a.anio > b.anio) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            renderizarAutos(autos)
+            agregarAlCarrito(autos)
+        })
+
+
+        let mayorAnio = document.getElementById("mayorAnio")
+
+        mayorAnio.addEventListener("click", () => {
+            autos.sort((a, b) => {
+                if (a.anio > b.anio) {
+                    return -1;
+                } else if (a.anio < b.anio) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            renderizarAutos(autos)
+            agregarAlCarrito(autos)
+        })
+
+
+        // ---- BUSCAR VEHICULO ----
+        const inputBusqueda = document.getElementById("busqueda")
+        const botonBuscar = document.getElementById("botonBuscar")
+        const resultados = document.getElementById("resultados")
+        let cards = document.getElementById("cards")
+        renderizarAutos(autos)
+
+
+
+        botonBuscar.onclick = () => {
+            cards.innerHTML = ''
+            let autosFiltrados = autos.filter(auto => auto.marca.includes(inputBusqueda.value.toLowerCase()))
+            renderizarAutos(autosFiltrados)
+            agregarAlCarrito(autosFiltrados)
+
         }
-    })
-    renderizarAutos(autos)
-})
+
+        // --------CARDS---------
 
 
-// Vehiculo menor kilometros
-
-let menorKilometros = document.getElementById("menorKilometros")
-
-menorKilometros.addEventListener("click", () => {
-    autos.sort((a, b) => {
-        if (a.kilometros < b.kilometros) {
-            return -1;
-        } else if (a.kilometros > b.kilometros) {
-            return 1;
-        } else {
-            return 0;
-        }
-    })
-    renderizarAutos(autos)
-})
-
-
-// Vehiculo mayor kilometros
-
-let menorAnio = document.getElementById("menorAnio")
-
-menorAnio.addEventListener("click", () => {
-    autos.sort((a, b) => {
-        if (a.anio < b.anio) {
-            return -1;
-        } else if (a.anio > b.anio) {
-            return 1;
-        } else {
-            return 0;
-        }
-    })
-    renderizarAutos(autos)
-})
-
-
-let mayorAnio = document.getElementById("mayorAnio")
-
-mayorAnio.addEventListener("click", () => {
-    autos.sort((a, b) => {
-        if (a.anio > b.anio) {
-            return -1;
-        } else if (a.anio < b.anio) {
-            return 1;
-        } else {
-            return 0;
-        }
-    })
-    renderizarAutos(autos)
-})
-
-// ---- BUSCAR VEHICULO ----
-const inputBusqueda = document.getElementById("busqueda")
-const botonBuscar = document.getElementById("botonBuscar")
-const resultados = document.getElementById("resultados")
-let cards = document.getElementById("cards")
-renderizarAutos(autos)
-
-
-botonBuscar.onclick = () => {
-    cards.innerHTML = ''
-    let autosFiltrados = autos.filter(auto => auto.marca.includes(inputBusqueda.value))
-    renderizarAutos(autosFiltrados)
-}
-
-// --------CARDS---------
-
-
-function renderizarAutos(autosFiltrados) {
-    cards.innerHTML=''
-    for (const auto of autosFiltrados) {
-        let tarjetaProducto = document.createElement('div')
-        tarjetaProducto.className = 'producto'
-        tarjetaProducto.innerHTML = `
-        <img class="imgCard cards" src=${auto.img}>
+        function renderizarAutos(autosFiltrados) {
+            cards.innerHTML = ''
+            for (const auto of autosFiltrados) {
+                let tarjetaProducto = document.createElement('div')
+                tarjetaProducto.className = 'producto'
+                tarjetaProducto.innerHTML = `
+        <img class="imgCard cards" src="${auto.img}">
         <p class="h3Card cards"><h5>Marca</h5> ${auto.marca}</p>
         <p class="h4Card cards"><h5>Modelo</h5> ${auto.modelo}</p>
         <p class="parrafoCard cards"><h5>Año</h5> ${auto.anio}</p>
         <p class="parrafoCard cards"><h5>Kilometros</h5> ${auto.kilometros}</p>
         <button class="boton" id=${auto.id}>Agregar al carrito</button>
         `
-        cards.append(tarjetaProducto)
-    }
-}
-
-let botones = document.getElementsByClassName("boton")
-let carritoContenedor = document.getElementById("carrito")
-
-let carritoCompra = []
-
-if (localStorage.getItem("carritoContenedor")) {
-    carritoCompra = JSON.parse(localStorage.getItem("carritoContenedor"))
-}
-
-
-retornarCarrito()
-
-
-for (const boton of botones) {
-    boton.onclick = (e) => {
-        let productoBuscado = autos.find(auto => auto.id == e.target.id)
-
-        let posicionProductoCarrito = carritoCompra.findIndex(auto => auto.id == productoBuscado.id)
-
-        if (posicionProductoCarrito != -1) {
-            carritoCompra[posicionProductoCarrito].Unidades++
-            carritoCompra[posicionProductoCarrito].Subtotal = carritoCompra[posicionProductoCarrito].precioUnidad * carritoCompra[posicionProductoCarrito].Unidades
-
-        } else {
-            carritoCompra.push({ id: productoBuscado.id, nombre: productoBuscado.marca, precioUnidad: productoBuscado.valor, Unidades: 1, Subtotal: productoBuscado.valor })
+                cards.append(tarjetaProducto)
+            }
         }
 
-        console.log(carritoCompra)
-        localStorage.setItem("carritoContenedor", JSON.stringify(carritoCompra))
+        let botones = document.getElementsByClassName("boton")
+        let carritoContenedor = document.getElementById("carrito")
+
+        let carritoCompra = []
+
+        if (localStorage.getItem("carritoContenedor")) {
+            carritoCompra = JSON.parse(localStorage.getItem("carritoContenedor"))
+        }
+
+
         retornarCarrito()
 
-        Swal.fire('¡Se agrego al carrito!')
-    }
-}
-function retornarCarrito() {
-    carritoContenedor.innerHTML = `
+        function agregarAlCarrito() {
+            for (const boton of botones) {
+                boton.onclick = (e) => {
+                    let productoBuscado = autos.find(auto => auto.id == e.target.id)
+
+                    let posicionProductoCarrito = carritoCompra.findIndex(auto => auto.id == productoBuscado.id)
+
+                    if (posicionProductoCarrito != -1) {
+                        carritoCompra[posicionProductoCarrito].Unidades++
+                        carritoCompra[posicionProductoCarrito].Subtotal = carritoCompra[posicionProductoCarrito].precioUnidad * carritoCompra[posicionProductoCarrito].Unidades
+
+                    } else {
+                        carritoCompra.push({
+                            id: productoBuscado.id,
+                            img: productoBuscado.img,
+                            nombre: productoBuscado.modelo,
+                            precioUnidad: productoBuscado.valor,
+                            Unidades: 1,
+                            Subtotal: productoBuscado.valor,
+                        })
+                    }
+
+                    console.log(carritoCompra)
+                    localStorage.setItem("carritoContenedor", JSON.stringify(carritoCompra))
+                    retornarCarrito()
+                    Swal.fire('¡Se agrego al carrito!')
+                }
+            }
+        }
+
+        function retornarCarrito() {
+            carritoContenedor.innerHTML = ``
+            let total = 0
+            for (const item of carritoCompra) {
+                total += item.Subtotal
+                carritoContenedor.innerHTML += `
         <div class="itemCarrito">
-        <p>Nombre</p>
-        <p>PrecioUnidad</p>
-        <p>Unidades</p>
-        <p>Subtotal</p>
-        </div>
-    `
-    let total = 0
-    for (const item of carritoCompra) {
-        total += item.Subtotal
-        carritoContenedor.innerHTML += `
-        <div class="itemCarrito">
-        <img class="imgCarrito" src=${item.img}>
-        <p>${item.nombre}</p>
-        <p>${item.precioUnidad}</p>
-        <p>${item.Unidades}</p>
-        <p>${item.Subtotal}</p>
+        <img class="imgCarrito" src="${item.img}">
+        <p class="parrafoCarrito">${item.nombre}</p>
+        <p class="parrafoCarrito">${item.precioUnidad}</p>
+        <p class="parrafoCarrito">${item.Unidades}</p>
+        <p class="parrafoCarrito">$${item.Subtotal}</p>
+        <button onClick = "eliminarProducto(${item.id})">Eliminar</button>
         </div>
         `
-    }
-    carritoContenedor.innerHTML += `
-        <div class="itemCarrito">
+            }
+
+            carritoContenedor.innerHTML += `
+        <div class="itemTotal">
         <h2>Total</h2>
         <p>${total}</p>
         </div>
         `
-}
+            agregarAlCarrito()
+        }
+
+    })
